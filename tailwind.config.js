162864f8c87964fd416628a2ab1fc0c8df45a5f3
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2023-10-23 14:26:15 +0630

    add required assest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kalam: ["Kalam", "cursive"],
+        raleway: ["Raleway", "sans-serif"],
+      },
+      colors: {
+        "primary-black": "#222",
+        "primary-green": "#94CA21",
+        "secondary-green": "#EDF3E0",
+        "accent-green": "#D3EB9F",
+        "light-gray": "#686868",
+        "dark-gray": "#414141",
+        "subtitle-gray": "#686868",
+        "lightest-gray": "#F9F9F9",
+        "accent-gray": "#E1E1E1",
+        "secondary-gray": "#E4E4E4",
+      },
+      screens: {
+        xl: "1140px",
+        lg: "900px",
+        sm: "640px",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
